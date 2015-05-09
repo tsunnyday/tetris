6c201b1858e9b34e3432c673e0b8454f39376d12
tsunnyday <collechess123@gmail.com>
2015-05-09 17:01:11 -0500

    Added adjusting
    So you can now move left and right as far as possible, and it'll adjust
    when you rotate.  Next I'll make them check below and solidify.

--- tetris.py
+++ tetris.py
@@ -3,6 +3,8 @@ from pygame.locals import *
 
 
 STEP_TIME = 600
+STAGE_WIDTH = 10
+STAGE_HEIGHT = 20
 
 # name: number of rotations, width/height (everything is stored in a square), each rotation
 pieces = {"i":[2, 4, [0,1,0,0,
@@ -71,29 +73,41 @@ class Piece:
 			self.index = pieces[self.kind][0] - 1
 		else:
 			self.index -= 1
+		self.adjust()
 	
 	def rot_right(self):
 		if self.index == pieces[self.kind][0] - 1:
 			self.index = 0
 		else:
 			self.index += 1
+		self.adjust()
 	
 	def advance(self):
 		self.y += 32
 		
 	def move_x(self, x_off):
 		self.x += x_off
-		if self.x < 0:
-			self.x = 0
-		if self.x + 32*pieces[self.kind][1] > 10*32:
-			self.x = 10*32 - 32*pieces[self.kind][1]
+		self.adjust()
+		
+	def adjust(self):
+		for i in range(self.get_size()):
+			if any(self.get_column(i)):
+				if self.x + 32*i < 0:
+					self.x = 0 - 32*i
+				break
+		for i in reversed(range(self.get_size())):
+			if any(self.get_column(i)):
+				if self.x + 32*(i+1) > 32*10:
+					self.x = 32*10 - 32*(i+1)
+				break
+			
+	
 			
 	def get_column(self, x):
-		print [self.get_matrix()[i*self.get_size()+x] for i in range(self.get_size())]
 		return [self.get_matrix()[i*self.get_size()+x] for i in range(self.get_size())]
 	
 	def get_row(self, y):
-		print [self.get_matrix()[y*self.get_size() + i] for i in range(self.get_size())]
+		return [self.get_matrix()[y*self.get_size() + i] for i in range(self.get_size())]
 	
 	
 	def get_pos(self):
@@ -126,6 +140,7 @@ pygame.display.update()
 
 test_piece = Piece(random.choice(pieces.keys()), 160, 0)
 step = 0
+stage = [0] * (10*20)
 
 while 1:
 	for event in pygame.event.get():
